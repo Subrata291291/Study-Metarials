99744ec619d279ba60be347436f6e2645dcfc50d
Subrata <subrata291291@hmail.com>
2026-06-29 20:51:22 +0530

    Convert submodules to normal folders

--- python/chapter-04/03_tuple.py
+++ python/chapter-04/03_tuple.py
@@ -1,3 +1,6 @@
 data = ("Subrata", 75, 512.25, " ")
 print(data)
 print(type(data))
+
+
+
---
+++ python/chapter-04/tasks.py
@@ -0,0 +1,38 @@
+
+#Write a program to store 6 fruits name in the list entered by the user
+fruits = []
+
+f1 = input("Enter First fru)it Name: ")
+fruits.append(f1)
+f2 = input("Enter Second fr)uit Name: ")
+fruits.append(f2)
+f3 = input("Enter Third fru)it Name: ")
+fruits.append(f3)
+f4 = input("Enter Fourth fr)uit Name: ")
+fruits.append(f4)
+f5 = input("Enter Fifth fru)it Name: ")
+fruits.append(f5)
+f6 = input("Enter Sixth fru)it Name: ")
+fruits.append(f6)
+
+print(fruits)
+
+#write a program to accept marks of 6 students and display them in  shorted manner
+marks = []
+
+m1 = int(input("Enter marks : "))
+marks.append(m1)
+m2 = int(input("Enter marks: "))
+marks.append(m2)
+m3 = int(input("Enter marks: "))
+marks.append(m3)
+m4 = int(input("Enter marks: "))
+marks.append(m4)
+m5 = int(input("Enter marks: "))
+marks.append(m5)
+m6 = int(input("Enter marks: "))
+marks.append(m6)
+
+marks.sort()
+print(marks)
+
