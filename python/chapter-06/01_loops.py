1579b1a8d7b6e48c4cf654f2161b44eb2899fcf8
Subrata <subrata291291@hmail.com>
2026-06-30 20:01:42 +0530

    Added Chapter 07

---
+++ python/chapter-06/01_loops.py
@@ -0,0 +1,41 @@
+# #for loop
+# for i in range(1,11):
+#     print(i) #Output 1-10
+
+
+
+#while loop (This block will execute until the condtion is true)
+# numbers = 1
+# while(numbers<11):
+#     print(numbers) #Output 1-10
+#     numbers = numbers + 1
+
+
+
+# names = [1, "Subrata", False, "Rohit", "Nandita"]
+# i = 0
+# while i < len(names):
+#     print(names[i])
+#     i = i + 1
+
+
+#for loop using else
+# number = [12,16,52,14,58]
+# for i in number:
+#     print(i)
+# else:
+#     print("done")
+
+
+#for loop using break
+# for i in range(100):
+#     if(i == 20):
+#         break #Exit the loop
+#     print(i)
+
+
+#for loop using break
+# for i in range(30):
+#     if(i == 20):
+#         continue #Skip the iteration(i)
+#     print(i)
---
+++ python/chapter-06/02_pass.py
@@ -0,0 +1,9 @@
+#One Loop
+for i in range(20):
+    pass # pass is mainly use to skip the function
+
+#Another Loop
+i = 0
+while(i<30):
+    print(i)
+    i = i+1
